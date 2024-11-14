d2ed79330cdb07f10ef5d22e93fb233844e1e9b0
InStageTwo <borzovadarian@yandex.ru>
2024-11-14 22:31:33 +0300

    feat: solve 3 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -33,10 +33,9 @@ function getRectangleArea(width, height) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
 }
-
 /**
  * Returns an average of two given numbers.
  *
@@ -49,8 +48,8 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return value1 / 2 + value2 / 2;
 }
 
 /**
