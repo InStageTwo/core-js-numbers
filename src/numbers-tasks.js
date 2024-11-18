6314423c470952aab037ff09a6ff238587a9d228
InStageTwo <borzovadarian@yandex.ru>
2024-11-18 11:32:48 +0300

    feat: solve 56 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -106,8 +106,10 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return (
+    Math.acos(x1 * x2 + y1 * y2) - (Math.sqrt(x1 * y1) + Math.sqrt(x2 * y2))
+  );
 }
 
 /**
@@ -565,8 +567,8 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(...args) {
+  return args.reduce((sum, current) => sum + current, 0).toFixed(2);
 }
 
 /**
@@ -628,8 +630,8 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  return Math.ceil(Math.abs(number) / 2);
 }
 
 module.exports = {
